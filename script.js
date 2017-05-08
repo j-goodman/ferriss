39c6cf625ae570c770ef9b56e83da2eb847107e5
John Goodman <john.goodman@joystickinteractive.com>
2017-05-08 12:59:49 -0400

    Add slide changing

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 window.onload = function () {
-  var currentSlide;
   var i;
   var slides;
+
   slides = document.getElementsByTagName('img');
-  currentSlide = 0;
+  document.currentSlide = 0;
   for (i=0 ; i<slides.length ; i++) {
     slides[i].index = i;
   }
-  updateSlides(slides, currentSlide);
+  updateSlides(slides, document.currentSlide);
+  setupSwitches(slides);
 };
 
 var updateSlides = function (slides, currentSlide) {
   for (i=0 ; i<slides.length ; i++) {
-    if (slides[i].index < currentSlide) {
+    if (slides[i].index < document.currentSlide) {
       slides[i].classList.add('above');
-    } else if (slides[i].index > currentSlide) {
+    } else if (slides[i].index > document.currentSlide) {
       slides[i].classList.add('below');
+    } else {
+      slides[i].className = '';
     }
   }
 };
 
-setupSwitches = function () {
+var setupSwitches = function (slides) {
+  var down;
+  var up;
 
+  up = document.getElementById('up');
+  down = document.getElementById('down');
+
+  up.onclick = function () {
+    if (document.currentSlide > 0) {
+      changeSlide(-1, slides);
+    }
+
+  };
+
+  down.onclick = function () {
+    if (document.currentSlide < slides.length) {
+      changeSlide(1, slides);
+    }
+
+  };
+};
+
+var changeSlide = function (direction, slides) {
+  document.currentSlide += direction;
+  updateSlides(slides, document.currentSlide);
 };
